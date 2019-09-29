37aedbb61d9f054afe5d960367153bf8dd5369da
AlyxiaFox <alyxiafox@gmail.com>
2019-09-29 14:16:14 -0700

    Added Slim UI

---
+++ Assets/Scripts/GameObjectDis.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectDis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InteractivityManager = GameObject.FindGameObjectWithTag("MixPlay");
+    }
+    public void HideGameObject()
+    {
+        if (InteractivityManager.activeInHierarchy)
+        {
+            InteractivityManager.SetActive(false);
+        }
+        else
+        {
+            InteractivityManager.SetActive(true);
+        }
+    }
+    public GameObject InteractivityManager;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
